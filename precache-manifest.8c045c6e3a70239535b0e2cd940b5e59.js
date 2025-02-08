aa6eb321c4146e2f3ce47e28980e965dd92f0176
AndrewDul <dulandrew.dev@gmail.com>
2025-02-08 00:48:18 +0000

    Updates

---
+++ precache-manifest.8c045c6e3a70239535b0e2cd940b5e59.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e058d9960ab9dcd89bc4e7a388b0d66b",
+    "url": "/Taskify/index.html"
+  },
+  {
+    "revision": "ee5708b54f6c62e01bd3",
+    "url": "/Taskify/static/css/main.718e496b.chunk.css"
+  },
+  {
+    "revision": "638e200e69d950262938",
+    "url": "/Taskify/static/js/2.ae026ab9.chunk.js"
+  },
+  {
+    "revision": "ee5708b54f6c62e01bd3",
+    "url": "/Taskify/static/js/main.9b3085d7.chunk.js"
+  },
+  {
+    "revision": "c63ce01dd015b3c6e864",
+    "url": "/Taskify/static/js/runtime~main.e19d72fd.js"
+  }
+]);
